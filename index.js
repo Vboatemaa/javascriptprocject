9e251f4aca3139bbdae59080e0d53a731ec4ec4d
Vboatemaa <vanessaboatemaa5@gmail.com>
2025-01-20 22:04:25 -0500

    second commit

--- index.js
+++ index.js
@@ -11,11 +11,36 @@ todos.push({ title, description, complete: false});
 console.log(`Added todo: ${title}`);
 }
 function removeTodo(index){
-  
+if (index >= 0 && index < todos.length) {
+const removeTodo = todos.splice(index, 1);
+console.log(`Removed todo: ${removedTodo[0].title}`);
+} else {
+console.log("Invalid index. Unable to remove todo.");
+} 
 }
-function editTodo(){}
-function markTodoComplete(){}
-function displayTodoLength(){}
+function editTodo(index, newTitle, newDescription) {
+  if (index >= 0 && index < todos.length){
+  todos[index].title = newTitle || todos[index].title;
+  todos[index].description = newDescription || todos[index].description;
+  console.log(`Edited todo at index ${index}`);
+} else {
+console.log("invalid index. Unable to edit todo.");
+}
+
+function markTodoComplete(index){
+  if (index >= 0 && index < todos.length){
+    todos[index].complete = true;
+    console.log(`Marked todo as complete: ${todos[index].title}`); 
+  } else {
+    console.log("Invalid index. Unable to mark todo as complete.");
+  }
+}
+function displayTodoLength(){
+  const totalTodos = todos.length;
+  const completedTodos = todos.filter((todo) => todo.complete).length;
+  console.log(`Total todos: ${totalTodos}, Completed todos: ${completedTodos}`);
+}
+
 
 function app(){
   console.log('Welcome to the Todo Application');
